7961eb71e1052dcc251a3263e502c4f49ec172c5
muddin3031 <32686654+muddin3031@users.noreply.github.com>
2018-10-17 08:27:59 -0400

    Update Circle.java

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -1,4 +1,21 @@
 package com.company;
 
-public class Circle {
+public class Circle extends Shape{
+    private int side;
+    public Circle(int side)
+    {
+        super(type:"circle");
+        this.side=side;
+    }
+    @Override
+    public double getArea()
+    {
+        return 3.14*side*side;
+
+    }
+  @Override
+    public double getPerimeter()
+    {
+        return 3.14*side*2;
+    }
 }
