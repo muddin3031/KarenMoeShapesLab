a902c87744fb966ecc0b09318e2fb6a946a44f37
muddin3031 <32686654+muddin3031@users.noreply.github.com>
2018-10-17 08:37:27 -0400

    Update ETriangle.java

--- src/com/company/ETriangle.java
+++ src/com/company/ETriangle.java
@@ -1,4 +1,21 @@
 package com.company;
 
-public class ETriangle {
+public class ETriangle extends Shape{
+  private int side;
+  public ETriangle(int side)
+  {
+    super(type:"ETriangle");
+    this.side=side;
+  }
+  @Override
+  public double getArea()
+  {
+    return 1/2*side*Math.sqrt(3)*side*1/2;
+    
+  }
+  @Override
+  public double getPerimeter()
+  {
+    return side*3;
+  }
 }
